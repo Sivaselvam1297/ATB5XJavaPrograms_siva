d6dc745ccf59dfde10d209de184cd28ca60321a4
ADMIN <sivaselvam1297@gmail.com>
2024-01-10 12:02:25 +0530

    First code

---
+++ src/Lab002.java
@@ -0,0 +1,21 @@
+package src;
+
+public class Lab002 {
+
+    public static void main(String[] args) {
+
+        //Keywords are in Lower Case
+        //We can't use variables as keywords
+
+        byte age = 10;  // range of -128 to 127
+        age =126;
+        System.out.println(age);
+        //Local variable =age
+
+        short _age=128;   // -32768 to 32768
+
+        System.out.println(_age);
+
+
+    }
+}
